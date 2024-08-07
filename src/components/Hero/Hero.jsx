9e2017f53f572cd3f222de5dbfee3d5b1d0bc544
saifalden1000 <samory01275865029@gmail.com>
2024-08-07 06:42:39 +0300

    ..

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,9 @@ export default function Hero() {
   return (
     <div className="min-h-screen bg-[#1abc9c]  flex justify-center items-center flex-col pt-20">
       <img src={avatar} className="size-64" alt="Avatar" />
-      <h2 className="text-5xl font-bold text-white mt-6">START FRAMEWORK</h2>
+      <h2 className="text-5xl text-center font-bold text-white mt-6">
+        START FRAMEWORK
+      </h2>
       <div className="flex justify-between gap-5 items-center text-xl mt-3">
         <div className="line w-20 h-1 bg-white"></div>
         <FontAwesomeIcon icon={faStar} className="text-white" />
