6fa8246adda7b9f8a0e6f7b2b5ab5dc5b2a85dd3
saifalden1000 <samory01275865029@gmail.com>
2024-08-07 20:27:33 +0300

    app fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./App.css";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Portfolio from "./components/Portfolio/Portfolio";
